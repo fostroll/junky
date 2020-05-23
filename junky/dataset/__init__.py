b039f06239420e50d376153af7245306cadfaad9
fostroll <fostroll@gmail.com>
2020-05-23 23:10:27 +0300

    new dataset

--- junky/dataset/__init__.py
+++ junky/dataset/__init__.py
@@ -8,3 +8,4 @@ Provides implementagions of torch.utils.data.Dataset for different purposes.
 """
 from junky.dataset.char_dataset import *
 from junky.dataset.token_dataset import *
+from junky.dataset.word_dataset import *
