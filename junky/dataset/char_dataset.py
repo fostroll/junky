5c3624f2c5bb9f4f3196c9b6770ca6dfc04488e4
fostroll <fostroll@gmail.com>
2020-05-25 13:42:42 +0300

    typo

--- junky/dataset/char_dataset.py
+++ junky/dataset/char_dataset.py
@@ -199,8 +199,8 @@ class CharDataset(Dataset):
         lens = [tensor([len(x) for x in x[idx]]) for x in batch]
         if self.min_len is not None:
             batch.append(([tensor([self.pad])] * self.min_len))
-        x = junky.([x[idx] for x in batch],
-                                  padding_value=self.pad)
+        x = pad_array_torch([x[idx] for x in batch],
+                            padding_value=self.pad)
         if self.min_len is not None:
             x = x[:-1]
         return x, lens
