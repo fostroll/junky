5d10d87475f806a2d2f12bd9be73ee9bbff64709
fostroll <fostroll@gmail.com>
2020-05-26 14:00:17 +0300

    minor update

--- junky/plotter.py
+++ junky/plotter.py
@@ -15,11 +15,9 @@ import matplotlib.style
 from matplotlib import pyplot as plt
 import seaborn as sns
 
-mpl.style.use('default')
 
-
-def plot_losses(train_losses, test_losses, accuracies=None, 
-                plot_title='Train/Dev Loss', figsize=(10,6), 
+def plot_losses(train_losses, test_losses, accuracies=None,
+                plot_title='Train/Dev Loss', figsize=(10, 6),
                 legend_labels=['train', 'dev', '1 - acc'], save_name=None):
     """Plots train and dev losses obtained during training.
     The plot image is saved to disk.
@@ -27,7 +25,8 @@ def plot_losses(train_losses, test_losses, accuracies=None,
       train_losses:   list of floats, train losses throughout epochs.
       test_losses:    list of floats, test losses throughout epochs.
       accuracies:     optional, list of floats, accuracies throughout epochs.
-                      Here, used to count (1 - accuracy). If `None`, not plotted.
+                      Here, used to count (1 - accuracy). If `None`, not
+                      plotted.
       plot_title:     plot title, `str`. Default value - 'Train/Dev Loss'.
       figsize:        the size of the figure plotted. Default size is `(5,3)`.
       save_name:      optional, if `None`, plot is not saved. 
@@ -36,6 +35,7 @@ def plot_losses(train_losses, test_losses, accuracies=None,
                       please specify extention in save_name as well. 
                       Example: save_name='plot.pdf'
     """
+    mpl.style.use('default')
     plt.figure(figsize=figsize)
     plt.plot([None] + train_losses)
     plt.plot([None] + test_losses)
@@ -50,9 +50,10 @@ def plot_losses(train_losses, test_losses, accuracies=None,
         plt.savefig(save_name, bbox_inches='tight')
     plt.show()
 
-def plot_confusion_matrix(y_true, y_pred, n_classes, pad_index=None, ymap=None,
-                          figsize=(28,10), show_total=['x', 'y'], show_zeros=True,
-                          plot_title='Confusion Matrix', save_name=None):
+def plot_confusion_matrix(y_true, y_pred, n_classes, pad_index=None,
+                          ymap=None, figsize=(28, 10), show_total=['x', 'y'],
+                          show_zeros=True, plot_title='Confusion Matrix',
+                          save_name=None):
     """
     Generate matrix plot of confusion matrix with pretty annotations.
     The plot image is saved to disk.
@@ -80,11 +81,14 @@ def plot_confusion_matrix(y_true, y_pred, n_classes, pad_index=None, ymap=None,
       save_name:     str: filename of figure file to save. 
                      if `None`, image is not saved to disk.
     """
-    # if pad_index is not None and does not exist in y_pred, it's excluded from labels
-    labels = [i if pad_index is not None and i!=pad_index and pad_index not in y_pred
-              else i
-              for i in range(0, n_classes) 
-             ]
+    # if pad_index is not None and does not exist in y_pred, it's excluded
+    # from labels
+    mpl.style.use('default')
+
+    labels = [i if pad_index is not None and i != pad_index
+                                         and pad_index not in y_pred else
+              i
+              for i in range(0, n_classes)]
     if ymap is not None:
         labels = [ymap[yi] for yi in labels]
     cm = confusion_matrix(y_true, y_pred)
@@ -126,7 +130,6 @@ def plot_confusion_matrix(y_true, y_pred, n_classes, pad_index=None, ymap=None,
     plt.title(plot_title)
     plt.show()
 
-
 def plot_metrics(metrics=[], 
                  labels=['accuracy', 'precision', 'recalls', 'f1_score'],
                  plot_title='Metrics', figsize=(10,6), save_name=None):
@@ -142,11 +145,10 @@ def plot_metrics(metrics=[],
       save_name:      str: filename of figure file to save. 
                       if `None`, image is not saved to disk.
     """
+    mpl.style.use('default')
     plt.figure(figsize=figsize)
-
     for metric in metrics:
         plt.plot([None] + metric)
-
     plt.grid()
     plt.title(plot_title)
     plt.xlabel('Epochs')
