63997b435ab301613d2816d85d43f4dad4a0474b
fostroll <fostroll@gmail.com>
2020-07-01 11:24:06 +0300

    minor ref

--- junky/dataset/__init__.py
+++ junky/dataset/__init__.py
@@ -6,9 +6,14 @@
 """
 Provides implementagions of torch.utils.data.Dataset for different purposes.
 """
+from junky.dataset.dummy_dataset import *
+from junky.dataset.len_dataset import *
+
+from junky.dataset.word_dataset import *
 from junky.dataset.bert_dataset import *
+from junky.dataset.word_cat_dataset import *
+
 from junky.dataset.char_dataset import *
 from junky.dataset.token_dataset import *
-from junky.dataset.word_dataset import *
 
 from junky.dataset.frame_dataset import *
