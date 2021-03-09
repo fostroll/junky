c605212dded6ee227a9e79fad573930b2bded22a
fostroll <fostroll@gmail.com>
2021-03-09 13:25:10 +0300

    +help

--- junky/dataset/label_dataset.py
+++ junky/dataset/label_dataset.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
-# junky lib: dataset.TokenDataset
+# junky lib: dataset.LabelDataset
 #
 # Copyright (C) 2020-present by Sergei Ternovykh, Anastasiya Nikiforova
 # License: BSD, see LICENSE for details
 """
-Provides implementation of torch.utils.data.Dataset for token-level input.
+Provides implementation of torch.utils.data.Dataset for label-level input.
 """
 from junky import CPU, make_token_dict
 from junky.dataset.base_dataset import BaseDataset
@@ -13,13 +13,13 @@ from torch import Tensor, float32, int64, stack, tensor, zeros
 
 class LabelDataset(BaseDataset):
     """
-    torch.utils.data.Dataset for token-level input.
+    torch.utils.data.Dataset for label-level input.
 
     Args:
         labels: list([str])|list[list([str])]).
-        unk_label: add a token for tokens that are not present in the
-            internal dict: str.
-        extra_labels: add tokens for any other purposes: list([str]).
+        unk_label: add a value for labels that are not present in the internal
+            dict: str.
+        extra_labels: add label values for any other purposes: list([str]).
         tensor_dtype: dtype for tensors' data: torch.dtype. Default depends on
             labels: for just list it is torch.int64 wereas for list of lists
             it is torch.float32.
@@ -39,12 +39,12 @@ class LabelDataset(BaseDataset):
     def fit(self, labels, unk_label=None, extra_labels=None):
         """Recreate the internal dict.
 
-        :param labels: sequences of tokens.
+        :param labels: sequences of label values.
         :type labels: list([str])|list[list([str])]).
-        :param unk_label: add a token for tokens that are not present in the
+        :param unk_label: add a value for labels that are not present in the
             dict.
         :type unk_label: str
-        :param extra_labels: add tokens for any other purposes.
+        :param extra_labels: add label values for any other purposes.
         :type extra_labels: list([str])
         """
         if unk_label:
@@ -60,8 +60,8 @@ class LabelDataset(BaseDataset):
         self.reconstruct_dict = {y: x for x, y in self.transform_dict.items()}
 
     def label_to_idx(self, label, skip_unk=False):
-        """Convert a token to its index. If the token is not present in the
-        internal dict, return index of unk token or None if it's not
+        """Convert a label value to its index. If the value is not present in
+        the internal dict, return index of unk label or None if it's not
         defined."""
         return self.transform_dict[label] \
                    if label in self.transform_dict else \
@@ -69,9 +69,9 @@ class LabelDataset(BaseDataset):
                None
 
     def idx_to_label(self, idx, skip_unk=False):
-        """Convert an index to the corresponding token. If the index is not
-        present in the internal dict, return unk token or empty string if it's
-        not defined or *skip_unk* is ``True``. If *skip_pad* is ``True``,
+        """Convert an index to the corresponding label value. If the index is
+        not present in the internal dict, return unk label or empty string if
+        it's not defined or *skip_unk* is ``True``. If *skip_pad* is ``True``,
         padding index will be replaced to empty string, too."""
         if isinstance(idx, Tensor):
             idx = idx.tolist()
@@ -82,15 +82,25 @@ class LabelDataset(BaseDataset):
 
     def transform(self, labels, skip_unk=False, keep_empty=False,
                   save=True, append=False):
-        """Convert *sentences* of tokens to the sequences of the corresponding
+        """Convert *labels* of str type to the sequences of the corresponding
         indices and adjust their format for Dataset. If *skip_unk* is
-        ``True``, unknown tokens will be skipped. If *keep_empty* is
-        ``False``, we'll remove sentences that have no data after converting.
+        ``True``, unknown labels will be skipped. If *keep_empty* is
+        ``False``, we'll remove rows that have no data after converting.
 
-        If *save* is ``True``, we'll keep the converted sentences as the
+        Corresponding indices are represented as is (int numbers) if *labels*
+        are of list([str]) type. Elsewise, if *labels* are of list of
+        list([str]) type (each row may contain several label values), the
+        indices are represented as multi-hot vectors.
+
+        The type of indices representation is exactly the tensor_dtype
+        specified in constructor. If ``None`` specified (default), torch.int64
+        is used for singleton-type labels and torch.float32 in the multi-hot
+        case.
+
+        If *save* is ``True``, we'll keep the converted labels as the
         Dataset source.
 
-        If *append* is ``True``, we'll append the converted sentences to the
+        If *append* is ``True``, we'll append the converted labels to the
         existing Dataset source. Elsewise (default), the existing Dataset
         source will be replaced. The param is used only if *save* is
         ``True``."""
@@ -123,11 +133,11 @@ class LabelDataset(BaseDataset):
             return data
 
     def reconstruct(self, ids, skip_unk=False, keep_empty=False):
-        """Convert *sequences* of indices in Dataset format to the sentences
-        of the corresponding tokens. If *skip_unk* is ``True``, unknown
-        indices will be skipped. If *skip_pad* is ``True``, padding will be
-        removed. If *keep_empty* is ``False``, we'll remove sentences that
-        have no data after converting."""
+        """Convert *sequences* of indices in Dataset format to the rows of the
+        corresponding label values. If *skip_unk* is ``True``, unknown indices
+        will be skipped. If *skip_pad* is ``True``, padding will be removed.
+        If *keep_empty* is ``False``, we'll remove labels that are empty after
+        converting."""
         return [l for l in (self.idx_to_label(i, skip_unk=skip_unk)
                                 for i in ids)
                     if keep_empty or l]
